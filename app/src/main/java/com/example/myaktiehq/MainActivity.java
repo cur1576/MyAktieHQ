6cd0eeae78b915b81a412d097a7c7e468123edc4
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-13 10:52:00 +0100

    test 1

---
+++ app/src/main/java/com/example/myaktiehq/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myaktiehq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreat: View ist fertig");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this,"Settings wurde gedrückt", Toast.LENGTH_SHORT).show();
+           //     TextView textView = findViewById(R.id.textView);
+           //     textView.setText("geändert");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
