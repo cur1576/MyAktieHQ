6e95c9a138a19c4ce946d81a3a2a362e0c74f445
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-14 13:55:58 +0100

    test 7

---
+++ app/src/main/java/com/example/myaktiehq/EinstellungenFragment.java
@@ -0,0 +1,30 @@
+package com.example.myaktiehq;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+
+public class EinstellungenFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        preference.setSummary(newValue.toString());
+        return false;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle saveInstanceState, String rootKey) {
+
+        setPreferencesFromResource(R.xml.preferences,rootKey);
+        Preference aktienlistePref = findPreference(getString(R.string.preference_aktienliste_key));
+        aktienlistePref.setOnPreferenceChangeListener(this);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String gespeicherteAktienliste = sharedPrefs.getString(aktienlistePref.getKey(), "");
+        onPreferenceChange(aktienlistePref, gespeicherteAktienliste);
+
+    }
+}
