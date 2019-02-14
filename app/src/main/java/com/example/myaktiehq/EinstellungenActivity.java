cb9ef8b9f32455dc120f9fb452295fa0ab22c21d
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-14 12:13:09 +0100

    test 6

--- app/src/main/java/com/example/myaktiehq/EinstellungenActivity.java
+++ app/src/main/java/com/example/myaktiehq/EinstellungenActivity.java
@@ -1,8 +1,10 @@
 package com.example.myaktiehq;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
 public class EinstellungenActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
@@ -12,11 +14,20 @@ public class EinstellungenActivity extends PreferenceActivity implements Prefere
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        Preference aktienlistePref = findPreference(getString(R.string.preference_aktienliste_key));
+        aktienlistePref.setOnPreferenceChangeListener(this);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String gespeicherteAktienliste = sharedPrefs.getString(aktienlistePref.getKey(), "");
+        onPreferenceChange(aktienlistePref, gespeicherteAktienliste);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+
+        preference.setSummary(newValue.toString());
+        return true;
     }
 }
 
