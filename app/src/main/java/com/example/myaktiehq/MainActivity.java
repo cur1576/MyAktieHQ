672deb3e1b571471ec2995bf5fc6562fc7b0d652
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-19 09:41:47 +0100

    commit 03

--- app/src/main/java/com/example/myaktiehq/MainActivity.java
+++ app/src/main/java/com/example/myaktiehq/MainActivity.java
@@ -16,9 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreat: View ist fertig");
+        Log.d(TAG, "onCreat: View ist fertig gebaut");
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: hier wird neu gestartet");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: kurz vor sichtbar werden");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: für interaktion bereit");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // hier sollten vorgenommene Änderungen in der Activity gespeichert werden
+        Log.d(TAG, "onPause: kurz vor in den Hintergrund gehen");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: jetzt im Hintergrund und angehalten");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+        // kappen von Datenbanken etc..
+        // hier alle Daten speichern, die noch benötigt werden!!
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
